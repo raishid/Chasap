4dd40e667daf4f5650a19afe380b9fad0d4bd6ca
fededav <67575679+raishid@users.noreply.github.com>
2025-08-26 16:27:09 -0400

    update env

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,13 @@
-//simple express server to run frontend production build;
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", function (req, res) {
-	res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-app.listen(3000);
 
+console.log('Starting server...');
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+	console.log('Serving index.html');
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = 3001;
+app.listen(port, () => console.log(`Server running on port ${port}`));
